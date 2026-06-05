181979dcda493b2758324050cef2367fb074e8b7
vadim <vadim.polinevskiy06@mail.ru>
2026-06-05 13:38:40 +0300

    Reapply "feat(db): add SQLite support and refactor error handling"
    This reverts commit d25bbb6132257273a3b76739b8a1645cd6b475b6.

--- cmd/WebApi/main.go
+++ cmd/WebApi/main.go
@@ -7,7 +7,9 @@ import (
 
 	"github.com/sad-cat-cmd/WebApi/internal/config"
 	"github.com/sad-cat-cmd/WebApi/internal/handlers"
-	"github.com/sad-cat-cmd/WebApi/internal/services"
+
+	//"github.com/sad-cat-cmd/WebApi/internal/services"
+	"github.com/sad-cat-cmd/WebApi/internal/services/data"
 )
 
 var pathConfigFile string = "config.json"
@@ -15,16 +17,17 @@ var pathConfigFile string = "config.json"
 func main() {
 	configuration, err := config.NewConfiguration(pathConfigFile)
 	if err != nil {
-		fmt.Println("Error starting server: \n\tuncorrectness read config file: %w", err.Error())
+		fmt.Printf("Error starting server: failed to read config file: %v\n", err)
 		return
 	}
 
-	services, err := services.NewProductService(configuration)
+	service, err := data.NewProductServiceSQLlite(configuration)
 	if err != nil {
 		fmt.Println("Error starting server: \n\t ", err.Error())
 		return
 	}
-	productHandler := handlers.NewProductHandler(services)
+	defer service.CloseDB()
+	productHandler := handlers.NewProductHandler(service)
 
 	http.HandleFunc("GET /products", productHandler.GetAllHandler)
 	http.HandleFunc("GET /products/", productHandler.SearchHandler)
